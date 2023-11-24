49d07be2e3cef73f3056904beb1e3f0a69f1da65
Viresh <vireshkoli00@gmail.com>
2023-11-24 18:54:21 +0530

    Product Card Done

--- src/MyComponents/Categories/Supplements/Product Data/Data.js
+++ src/MyComponents/Categories/Supplements/Product Data/Data.js
@@ -1,4 +1,5 @@
 import muscleblazeprotein from "./muscleblazeprotein.jpg"
+import benchpress from "./benchpress.jpg"
 
 export const PRODUCTDATA = [
     {
@@ -7,5 +8,12 @@ export const PRODUCTDATA = [
         producttitle : "MuscleBlaze Whey Protein Powder 500g",
         productrating : "4.5",
         productprice : "3500 Rs"
+    },
+    {
+        productimage : benchpress,
+        productcategory : "EQUIPMENT",
+        producttitle : "Bench Press Prime",
+        productrating : "4.6",
+        productprice : "10999 Rs"
     }
 ]
