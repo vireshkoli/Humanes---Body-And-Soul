6d9f4ce0987ba6169c543912c260d6d12f2b767a
Viresh <vireshkoli00@gmail.com>
2023-12-09 00:07:43 +0530

    Sign Up Gym Done

--- src/MyComponents/Login Signup/LoginSignup.jsx
+++ src/MyComponents/Login Signup/LoginSignup.jsx
@@ -12,26 +12,149 @@ import profile from "./profile.png";
 function LoginSignup() {
   const [action, setAction] = useState("Sign Up");
 
+  let profileimagecontent = (
+    <div className="profileimage">
+      <img src={profile} alt="" />
+      <label for="profileimg">
+        {action === "Sign Up Gym" ? "Upload Logo" : "Upload Profile"}
+      </label>
+      <input
+        type="file"
+        id="profileimg"
+        accept="image/png, image/jpeg"
+        style={{
+          display: "none",
+        }}
+      />
+    </div>
+  );
+
+  let signuptrainercontent = (
+    <>
+      <div className="input aboutme">
+        <img src={person} alt="" />
+        <textarea
+          placeholder="Write About Yourself For Trainer's Description"
+          name="aboutme"
+          id="aboutme"
+          cols="30"
+          rows="10"
+          maxLength={500}
+          minLength={10}
+        ></textarea>
+      </div>
+      <div className="sessionamount">
+        <img src={money} alt="" />
+        <input type="number" placeholder="Enter Amount Per Session :" />
+      </div>
+      <div className="contactno">
+        <img src={person} alt="" />
+        <input type="tel" placeholder="Contact No." />
+      </div>
+      <div className="uploadfiles">
+        <img src={files} alt="" />
+        <div>
+          <label for="transformationfiles">Upload Transformations</label>
+          <input
+            type="file"
+            id="transformationfiles"
+            name="files[]"
+            multiple
+            accept="image/png, image/jpeg"
+            style={{
+              display: "none",
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+
+  let signupgymcontent = (
+    <>
+      <div className="input">
+        <img src={person} alt="" />
+        <input type="text" placeholder="Gym Name" />
+      </div>
+      <div className="input gymheadline">
+        <img src={person} alt="" />
+        <textarea
+          placeholder="Write Your Gym Headline 0 - 50 Alphabets"
+          name="aboutgym"
+          id="aboutgym"
+          cols="30"
+          rows="10"
+          maxLength={50}
+          minLength={10}
+        ></textarea>
+      </div>
+      <div className="input gymaddress">
+        <img src={person} alt="" />
+        <textarea
+          placeholder="Gym Address"
+          name="aboutgym"
+          id="aboutgym"
+          cols="30"
+          rows="10"
+          maxLength={100}
+          minLength={10}
+        ></textarea>
+      </div>
+      <div className="input">
+        <img src={person} alt="" />
+        <input type="text" placeholder="Enter Gym Services" />
+      </div>
+      <div className="input gymdesc">
+        <img src={person} alt="" />
+        <textarea
+          placeholder="Enter Gym Description"
+          name="aboutgym"
+          id="aboutgym"
+          cols="30"
+          rows="10"
+          maxLength={500}
+          minLength={10}
+        ></textarea>
+      </div>
+      <div className="input">
+        <img src={person} alt="" />
+        <input type="number" placeholder="Gym Membership Amount:" />
+      </div>
+      <div className="uploadfiles">
+        <img src={files} alt="" />
+        <div>
+          <label for="gymimages">Upload Gym Images</label>
+          <input
+            type="file"
+            id="gymimages"
+            name="files[]"
+            multiple
+            accept="image/png, image/jpeg"
+            style={{
+              display: "none",
+            }}
+          />
+          <span>(Aspect Ratio : 16/9)</span>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <div className="loginsignupcontainer">
       <div className="loginsignupheader inputslideright">
         <h1>{action}</h1>
         <div className="underline"></div>
       </div>
+
       <div className="loginsignupinputs inputslideright">
-        <div className="profileimage">
-          <img src={profile} alt="" />
-          <label for="profileimg">Upload Profile</label>
-          <input
-            type="file"
-            id="profileimg"
-            accept="image/png, image/jpeg"
-            style={{
-              display : "none"
-            }}
-          />
-        </div>
-        {action === "Login" ? (
+        {action === "Sign Up" ||
+        action === "Sign Up Trainer" ||
+        action === "Sign Up Gym" ? (
+          <>{profileimagecontent}</>
+        ) : undefined}
+
+        {action === "Login" || action === "Sign Up Gym" ? (
           <div></div>
         ) : (
           <div className="input">
@@ -39,58 +162,30 @@ function LoginSignup() {
             <input type="text" placeholder="Name" />
           </div>
         )}
-        <div className="input">
-          <img src={email} alt="" />
-          <input type="email" placeholder="Email" />
-        </div>
-        <div className="input">
-          <img src={password} alt="" />
-          <input type="password" placeholder="Password" />
-        </div>
-        {action === "Sign Up Trainer" ? (
+        {action === "Login" ||
+        action === "Sign Up" ||
+        action === "Sign Up Trainer" ? (
           <>
-            <div className="input aboutme">
-              <img src={person} alt="" />
-              <textarea
-                placeholder="Write About Yourself For Trainer's Description"
-                name="aboutme"
-                id="aboutme"
-                cols="30"
-                rows="10"
-                maxLength={500}
-                minLength={10}
-              ></textarea>
+            <div className="input">
+              <img src={email} alt="" />
+              <input type="email" placeholder="Email" />
             </div>
-            <div className="sessionamount">
-              <img src={money} alt="" />
-              <input type="number" placeholder="Enter Amount Per Session :" />
-            </div>
-            <div className="contactno">
-              <img src={person} alt="" />
-              <input type="tel" placeholder="Contact No." />
-            </div>
-            <div className="uploadtransformations">
-              <img src={files} alt="" />
-              <div className="uploadfiles">
-                <label for="transformationfiles">Upload Transformations</label>
-                <input
-                  type="file"
-                  id="transformationfiles"
-                  name="files[]"
-                  multiple
-                  accept="image/png, image/jpeg"
-                  style={{
-                    display : "none"
-                  }}
-                />
-              </div>
+            <div className="input">
+              <img src={password} alt="" />
+              <input type="password" placeholder="Password" />
             </div>
           </>
         ) : undefined}
 
-        {action === "Sign Up" || action === "Sign Up Trainer" ? (
-          <>
-            <div className="signuptext">
+        {action === "Sign Up Trainer" ? <>{signuptrainercontent}</> : undefined}
+
+        {action === "Sign Up Gym" ? <>{signupgymcontent}</> : undefined}
+
+        {action === "Sign Up" ||
+        action === "Sign Up Trainer" ||
+        action === "Sign Up Gym" ? (
+          <div className="signuptype">
+            <div className="signuptext" id="loginhere">
               Already Have An Account?{" "}
               <span onClick={() => setAction("Login")}>Login Here!</span>
             </div>
@@ -102,29 +197,27 @@ function LoginSignup() {
                 </span>
               </div>
             ) : undefined}
-          </>
+            {action === "Sign Up" ? (
+              <div className="signuptext">
+                Are You A Gym Owner ?{" "}
+                <span onClick={() => setAction("Sign Up Gym")}>
+                  Sign Up As A Gym
+                </span>
+              </div>
+            ) : undefined}
+          </div>
         ) : undefined}
+
         {action === "Login" ? (
           <div className="forgot-password">
             Lost Password ? <span>Click Here!</span>
           </div>
         ) : undefined}
-        <div className="submitcontainer">
-          {/* For Sign Up And Login Buttons To be together */}
-          {/* <div
-            className={action === "Login" ? "submit gray" : "submit"}
-            onClick={() => setAction("Sign Up")}
-          >
-            Sign Up
-          </div>
-          <div
-            className={action === "Sign Up" ? "submit gray" : "submit"}
-            onClick={() => setAction("Login")}
-          >
-            Login
-          </div> */}
 
-          {action === "Sign Up" || action === "Sign Up Trainer" ? (
+        <div className="submitcontainer">
+          {action === "Sign Up" ||
+          action === "Sign Up Trainer" ||
+          action === "Sign Up Gym" ? (
             <div className="submit">Sign Up</div>
           ) : (
             <>
